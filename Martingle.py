b9222bc5906566a5d8dfeca2d315bacf6a210beb
kxxfxm <34667975+kxxfxm@users.noreply.github.com>
2021-10-17 21:44:41 +0800

    Update Martingle.py
    wws
    V0.2--Object Oriental

--- Martingle.py
+++ Martingle.py
@@ -16,6 +16,62 @@ class FLAG(Enum):
     LOSS = False
     PROFIT = True
     
+class Position():
+    '''
+    initBalance: init balance
+    pos: position
+    R: Time for reborn
+    '''
+    def __init__(self, initBalance, pos, R:int=50):
+        self.initBalance = initBalance
+        self.__balance = initBalance
+        self.__pos = pos
+        self.__R = R
+        self.__t = 0
+        
+    @property
+    def balance(self) -> float:
+        return self.__balance
+    
+    @property
+    def pos(self) -> float:
+        return self.__pos
+    
+    @pos.setter
+    def pos(self, value):
+        self.__pos = value
+    
+    @property
+    def timeRemain(self) -> int:
+        return self.__t
+    
+    def getReturn(self, market) -> float:
+        '''
+        market: The current marketing price
+        --------------------
+        The operation will stop if you sign the account "dead", and will cost {R:int=50} times to reborn
+        --------------------
+        return: The return with current balance, position and market
+        '''
+        if 0 < self.__t:
+            logging.info("Im dead and waiting for reborn, {:d} remained".format(self.__t))
+            self.__t -= 1
+            if self.__t == 0:
+                if 0 < market:
+                    logging.info("Im back!")
+                else:
+                    logging.info("Market is not positive, one more wait!")
+                    self.__t += 1
+            return 0
+                    
+        pnl = self.initBalance * self.__pos * market
+        self.__balance = pnl + self.__balance
+        return pnl
+    
+    def dead(self):
+        self.__t = self.__R
+        
+        
 class Martingale(MCSimulation):
     '''
     子类实现相关加仓算法
@@ -29,7 +85,7 @@ class Martingale(MCSimulation):
     X: 加仓倍数
     R: Time for reborn
     L: Reach the limitation stop doubling the position and reinit
-    Flag: 盈利 (PROFIT) 亏损情况(LOSS)
+    Flag: 盈利 (PROFIT) 或 亏损(LOSS) 加仓
     '''
     def __init__(self,
                  InitBalance: float=100, 
@@ -48,44 +104,34 @@ class Martingale(MCSimulation):
         self.__L = L
         self.__Flag = Flag
 
-    def game(self) -> List[float]:
+    def game(self, mainBrunch=True) -> List[float]:
         # init status
-        stat = {
-            't': 0,
-            'count': 0,
-            'balance': self.balance,
-            'pos': self.initPos
-        }
+        p = Position(self.balance, self.initPos, self.__R)
+        count = 0
         ret = []
         logging.info('first trade')
         while True:
-            if 0 < stat['t']:
-                logging.info("Im dead and waiting for reborn, {:d} remained".format(stat['t']))
-                if stat['t']:
-                    stat['t'] -= 1
-                    continue
-                else:
-                    logging.info("Im back!")
-                
             # marketing price
             market = simu()
             # calc returns
-            pnl = self.balance * stat['pos'] * market
-            ret.append(pnl)
-            # update status
-            stat['balance'] = pnl + self.balance
-            logging.info('pos: {:.0%}, pnl: {:.2f}, balance: {:.2f}.'.format(stat['pos'], pnl, stat['balance']))
-            if (pnl > 0)!=self.__Flag.value or self.__L <= stat['count']:
-                logging.info('return total pnl: {:.2f}'.format(stat['balance'] - self.balance))
-                return ret    
-            else:
-                stat['count'] += 1
-                stat['pos'] *= self.__X
-
-            if self.__A == stat['count']: 
-                stat['t'] = self.__R
+            pnl = p.getReturn(market)  # return 0 if trigger 斗转星移
+            if pnl:
+                ret.append(pnl)
+                logging.info('pos: {:.0%}, pnl: {:.2f}, balance: {:.2f}.'.format(p.pos, pnl, p.balance))
+                # 策略
+                if (pnl > 0)!=self.__Flag.value or self.__L <= count:
+                    logging.info('return total pnl: {:.2f}'.format(p.balance - self.balance))
+                    return ret
+                else:
+                    count += 1
+                    p.pos *= self.__X  # 仓位调整
+                    
+                if self.__A == count:  # trigger 斗转星移 
+                    p.dead()
+                    
+                    
+                    
                 
 if __name__ == '__main__':
-
-    m = Martingale(Flag=FLAG.LOSS)  # double the position when loss
+    m = Martingale(Flag=FLAG.LOSS,L=7)  # double the position when loss
     m.getStat()
